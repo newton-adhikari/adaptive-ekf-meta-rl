e222e342fb6a49b466a8331b0405f735b240fe11
Newton Adhikari <newtonadk@gmail.com>
2026-06-01 19:34:47 +0545

    created calibrated task distribution for real sensor noise

---
+++ meta_rl/tasks/task_distribution.py
@@ -0,0 +1,15 @@
+"""
+This is for task distribution definitions for meta-RL training.
+Defines how training tasks are sampled, including calibrated distributions
+anchored to real sensor noise models.
+"""
+
+class CalibratedTaskDistribution:
+    """Task distribution centered on real sensor noise with controlled spread.
+    """
+
+    def __init__(
+        self,
+        
+    ):
+        pass
