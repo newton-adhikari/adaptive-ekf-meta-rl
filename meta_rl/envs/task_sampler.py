72889c1bb87d8e65152d009c5f0fc49799d2bdc0
Newton Adhikari <newtonadk@gmail.com>
2026-06-01 19:09:43 +0545

    create task sampler for meta rl training

---
+++ meta_rl/envs/task_sampler.py
@@ -0,0 +1,10 @@
+"""
+Task distribution sampler for meta-RL training.
+"""
+
+
+class TaskSampler:
+    """Samples tasks from a calibrated distribution for meta-RL training."""
+
+    def __init__(self, config: Optional[dict] = None):
+        pass
