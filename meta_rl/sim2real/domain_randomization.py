670336daed40036f8390274f855878967ec38333
Newton Adhikari <newtonadk@gmail.com>
2026-06-10 18:46:15 +0545

    created pipeline for sim2real

---
+++ meta_rl/sim2real/domain_randomization.py
@@ -0,0 +1,7 @@
+"""
+Need to Calibrate domain randomization for sim-to-real transfer.
+
+PLAN::::
+The training task distribution is taken as real sensor noise models
+collected on the target platform, not arbitrary ranges.
+"""
