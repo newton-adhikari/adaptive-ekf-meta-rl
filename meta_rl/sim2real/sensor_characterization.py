fce1ea242c0146c20a4dbce4bfa4b5edf21aa2de
Newton Adhikari <newtonadk@gmail.com>
2026-06-12 13:24:28 +0545

    created class for AllanVarianceResult

--- meta_rl/sim2real/sensor_characterization.py
+++ meta_rl/sim2real/sensor_characterization.py
@@ -3,3 +3,18 @@ Sensor characterization: Allan variance analysis, noise model fitting.
 
 This will be used to build calibrated task distributions from real sensor data.
 """
+
+import numpy as np
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class AllanVarianceResult:
+    """Results from Allan variance analysis."""
+    taus: np.ndarray           # averaging times
+    adev: np.ndarray           # Allan deviation values
+    arw: float                 # angle random walk (slope = -0.5)
+    bias_instability: float    # bias instability (minimum of adev)
+    rrw: float                 # rate random walk (slope = +0.5)
+
