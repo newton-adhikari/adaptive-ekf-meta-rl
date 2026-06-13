687773e8e32936e443312b4d851c498b5d00192a
Newton Adhikari <newtonadk@gmail.com>
2026-06-13 21:37:27 +0545

    added imu and odom callback

--- ros2_ws/ekf_node.py
+++ ros2_ws/ekf_node.py
@@ -8,6 +8,9 @@ Subscribes to IMU + odometry, publishes filtered state.
 import numpy as np
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64MultiArray
 
 class EKFNode(Node):
     """ROS2 EKF node for state estimation."""
@@ -15,6 +18,29 @@ class EKFNode(Node):
     def __init__(self):
         super().__init__("ekf_node")
 
+
+def imu_callback(self, msg: Imu):
+    """
+    here i use IMU for prediction step.
+    """
+    u = np.array([
+        msg.linear_acceleration.x,
+        msg.linear_acceleration.y,
+        msg.angular_velocity.z,
+    ])
+    self.ekf.predict(u)
+
+def odom_callback(self, msg: Odometry):
+    """
+    here i use odometry position for update step.
+    """
+    z = np.array([
+        msg.pose.pose.position.x,
+        msg.pose.pose.position.y,
+    ])
+    self.ekf.update(z)
+    self._publish_state(msg.header)
+    self._publish_diagnostics()
 
 def main(args=None):
     rclpy.init(args=args)
